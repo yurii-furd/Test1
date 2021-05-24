520d930d656520764065555da42a2f6840316679
Yurii Furd <yura.furd@gmail.com>
2021-05-24 15:15:29 +0300

    delete equals()

--- src/main/java/com/Person.java
+++ src/main/java/com/Person.java
@@ -23,12 +23,4 @@ public class Person {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(name, person.name);
-    }
-
 }
